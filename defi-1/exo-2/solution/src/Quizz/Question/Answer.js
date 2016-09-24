4cfbd891fcfb8e83a615788bf3f65c96a359f9c2
Julien Pradet <julien@occitech.fr>
2016-09-24 17:57:31 +0200

    Add defi-1/exo-2 solution
    L'idée est de vraiment pousser la décomposition au maximum. Cela permet d'avoir des composants de haut niveau plus lisibles et d'isoler chaque partie de l'application.
    
    Le fichier src/Quizz/Question/AnswerList est particulièrement intéressant étant donné que le mapping de données en composants React est un pattern très fréquent.

---
+++ defi-1/exo-2/solution/src/Quizz/Question/Answer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Answer () {
+  return (
+    <label>
+      <input type="radio" name="answer" value="alouate" />
+      Alouate
+    </label>
+  )
+}
---
+++ defi-1/exo-2/solution/src/Quizz/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Question from './Question'
+
+export default function Quizz () {
+  return (
+    <Question />
+  )
+}
